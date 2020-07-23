9bfec634697bcf5421cbab85992ab3c2c2c06477
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-07-23 18:35:37 +0200

    wip

--- cypress/integration/quotes.spec.js
+++ cypress/integration/quotes.spec.js
@@ -1,1 +1,14 @@
 // write tests here
+describe('Inputs and submit button', () => {
+  it('can navigate to the site', () => {
+    // this is one test
+  })
+
+  it('submit button is disabled', () => {
+    // this is another test
+  })
+})
+
+describe('Adding a new quote', () => {
+  // tests here
+})
