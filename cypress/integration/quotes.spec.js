7f0d0ef138c2b67e0ce33275fd4d8be3bb60d964
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-07-23 18:39:12 +0200

    wip

--- cypress/integration/quotes.spec.js
+++ cypress/integration/quotes.spec.js
@@ -1,7 +1,10 @@
 // write tests here
 describe('Inputs and submit button', () => {
   it('can navigate to the site', () => {
-    // this is one test
+    // this is setup for the actual test
+    cy.visit('http://localhost:1234')
+    // assert that the site we landed at is the correct one
+    cy.url().should('include', 'localhost')
   })
 
   it('submit button is disabled', () => {
