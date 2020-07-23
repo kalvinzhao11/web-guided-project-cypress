09ed997d0067c45a6c1a928f5192ebae216699d8
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-07-23 19:27:16 +0200

    wip

--- cypress/integration/quotes.spec.js
+++ cypress/integration/quotes.spec.js
@@ -62,5 +62,6 @@ describe('Adding and deleting a new quote', () => {
   it('can delete the newly added quote', () => {
     // find the delete button of the quote, click it,
     // assert that the contents are not in there any more
+    cy.contains('Have Fun!').siblings('button:nth-of-type(2)').click()
   })
 })
